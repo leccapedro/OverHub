e2a24e90b7ff9f466ba3eae7ce7dd0561499aa0a
RisasDev <mauriciouch15@gmail.com>
2025-07-06 20:03:17 -0400

    more configurable messages

--- src/main/java/studio/overmine/overhub/listeners/PlayerListener.java
+++ src/main/java/studio/overmine/overhub/listeners/PlayerListener.java
@@ -9,8 +9,6 @@ import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
 public class PlayerListener implements Listener {
